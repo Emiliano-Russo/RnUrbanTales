1beafb93a826cb559e682556a7bca51e2ae29bf8
Emiliano Russo <russo.emiliano97@gmail.com>
2023-12-21 23:08:52 -0300

    fix google maps ios key

--- ios/RnUrbanTales/AppDelegate.mm
+++ ios/RnUrbanTales/AppDelegate.mm
@@ -2,10 +2,13 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyCJZehqyI9VVg2TIaOaS1wttjMJBgiXYrY"];
   self.moduleName = @"RnUrbanTales";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
@@ -29,3 +32,4 @@
 }
 
 @end
+
