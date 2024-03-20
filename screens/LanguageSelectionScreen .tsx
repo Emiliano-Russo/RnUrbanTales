444235a31e0b1eb7a295975333c154d763e18fc1
Emiliano Russo <russo.emiliano97@gmail.com>
2024-03-20 19:22:05 -0300

    language auto selected

--- screens/LanguageSelectionScreen .tsx
+++ screens/LanguageSelectionScreen .tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, TouchableOpacity, ScrollView, StyleSheet, Image, ImageBackground } from 'react-native';
+import {
+  View,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  ActivityIndicator,
+} from 'react-native';
 import { Text } from '../tool-components/index';
 import { useTranslation } from 'react-i18next';
 import { availableLanguages } from '../i18next';
@@ -12,6 +20,7 @@ import { setSelectedLanguageAsync } from '../redux/userSlice';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../App';
+import * as RNLocalize from 'react-native-localize';
 
 const backgroundImage = require('../assets/background1.png');
 
@@ -20,6 +29,11 @@ export const LanguageSelectionScreen = () => {
   const dispatch: AppDispatch = useDispatch();
   const user = useSelector((state: RootState) => state.user);
   const navigation = useNavigation<StackNavigationProp<RootStackParamList, 'LanguageSelection'>>();
+
+  // Obtener el primer idioma de la lista de idiomas del dispositivo
+  const firstLocale = RNLocalize.getLocales()[0].languageTag;
+
+  console.log(firstLocale); // Ejemplo: 'en-US'
 
   React.useEffect(() => {
     if (user.selectedLanguage) {
@@ -31,45 +45,23 @@ export const LanguageSelectionScreen = () => {
     }
   }, [user.selectedLanguage, user.hasSeenWelcomeModal, navigation]);
 
+  React.useEffect(() => {
+    const lng = firstLocale.slice(0, 2);
+    if (lng == 'es' || lng == 'en' || lng == 'pt') handleLanguageChange(lng);
+    else handleLanguageChange('en');
+  }, []);
+
   const handleLanguageChange = async language => {
     i18n.changeLanguage(language);
     dispatch(setSelectedLanguageAsync(language));
+    console.log('we dispatched the language: ', language);
     navigation.navigate('Welcome');
   };
 
   return (
     <View style={styles.container}>
       <LinearGradient colors={['#7D3C98', '#191970']} style={styles.modalView}>
-        <ImageBackground
-          source={backgroundImage}
-          style={{
-            position: 'absolute', // Posiciona la imagen de fondo absolutamente
-            width: '100%', // Asegura que la imagen de fondo llene el contenedor
-            height: '100%', // Asegura que la imagen de fondo llene el contenedor
-            opacity: 0.2,
-          }}
-        />
-        <Animatable.View animation="slideInDown" duration={3000}>
-          <Image style={{ width: 200, height: 200 }} source={require('../assets/official-logo-white.png')} />
-        </Animatable.View>
-
-        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
-          {availableLanguages.map((language, index) => (
-            <Animatable.View
-              key={index}
-              animation="fadeInLeft"
-              duration={1800}
-              delay={index * 200} // Esto creará un efecto escalonado
-            >
-              <TouchableOpacity style={styles.languageButton} onPress={() => handleLanguageChange(language.code)}>
-                <Image style={styles.flag} source={{ uri: language.flagUri }} />
-                <Text fontType="regularFont" style={styles.languageText}>
-                  {language.name}
-                </Text>
-              </TouchableOpacity>
-            </Animatable.View>
-          ))}
-        </ScrollView>
+        <ActivityIndicator size="large" color="#0000ff" />
       </LinearGradient>
     </View>
   );
